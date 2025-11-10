17bee43bbb7386e446817cf9a0594f0438692fcb
tabant99-neo <tabant99@gmail.com>
2025-11-10 22:33:20 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,20 +1,13 @@
 """
-Russian Exam Grader Package
-Автоматическая система оценки экзаменационных ответов
+Sigma Case - Automatic Russian Exam Grading System
 """
 
-__version__ = "1.0.0"
-__author__ = "Sigma Case"
-__description__ = "GPU-ускоренная система оценки письменных ответов на русском языке"
+__version__ = "0.1.0"
+__author__ = "Your Name"
+__description__ = "Automatic grading system for Russian language exams using ML"
 
-# Импорты для удобного доступа
-from .grader import RussianExamGraderGPU
-from .utils import clean_html, preprocess_data_fast, safe_read_csv, finalize_score_vectorized
+from .app import main
+from .grader import RussianExamGrader
+from .utils import clean_html_simple, safe_read_csv
 
-__all__ = [
-    'RussianExamGraderGPU', 
-    'clean_html', 
-    'preprocess_data_fast', 
-    'safe_read_csv',
-    'finalize_score_vectorized'
-]
+__all__ = ['main', 'RussianExamGrader', 'clean_html_simple', 'safe_read_csv']

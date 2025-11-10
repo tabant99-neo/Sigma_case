994fa3ffd8804472c839007462669f1470f0e3a4
tabant99-neo <tabant99@gmail.com>
2025-11-10 21:41:39 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,14 +6,9 @@ with open("requirements.txt") as f:
 setup(
     name="sigma_case",
     version="1.0.0",
-    description="Russian Exam Grader System",
-    author="Your Name",
+    description="GPU-accelerated Russian Exam Grader System",
+    author="Sigma Case",
     packages=find_packages(),
     install_requires=requirements,
     python_requires=">=3.8",
-    entry_points={
-        'console_scripts': [
-            'sigma-case=run:main',
-        ],
-    },
 )
